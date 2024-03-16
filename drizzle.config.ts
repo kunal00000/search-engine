a21dd965c3ec57d28afbff06503c6e1c694b19ce
kunal00000 <kunalverma2468@gmail.com>
2024-03-16 17:13:28 +0530

    drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from 'drizzle-kit';
+import { DATABASE_URL } from '@/lib/config';
 
 export default {
   driver: 'pg',
   schema: './src/db/schema.ts',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    connectionString: DATABASE_URL,
   },
   out: './drizzle',
 } satisfies Config;
